b12eee68b14a11f0392b09f4728393a358b065ed
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-03-06 22:19:13 -0300

    fixing some errors

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 if [[ $1 -eq "x86_64" ]]; then
-    ./build.sh x86_64
-
+    ./scripts/x86_64/build.sh
     echo "building test"
     gcc tests/m.c -lm -I./include final/x86_64.a -o final/test
 fi
 
 if [[ $1 -eq "aarch64" ]]; then
-    ./build.sh aarch64
-
+    ./scripts/aarch64/build.sh
     echo "building test"
     clang tests/m.c -I./include final/aarch64.a -o final/test
 fi
