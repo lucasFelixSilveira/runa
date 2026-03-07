b9a0af493a04b511c0e6e05f1ea5e72fbef18941
LucasFelixSilveira <lucasdwbfff@gmail.com>
2026-03-06 22:19:21 -0300

    fixing some errors 2

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ fi
 if [[ $1 -eq "aarch64" ]]; then
     ./scripts/aarch64/build.sh
     echo "building test"
-    clang tests/m.c -I./include final/aarch64.a -o final/test
+    clang tests/m.c -lm -I./include final/aarch64.a -o final/test
 fi
 
 echo "running test"
